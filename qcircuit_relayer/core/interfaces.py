d07289026e39086c3726aff7316c779f25fef129
itaborala <28213905+Itaborala@users.noreply.github.com>
2026-05-08 17:34:45 +0200

    initial file and class structure planning

---
+++ qcircuit_relayer/core/interfaces.py
@@ -0,0 +1,13 @@
+from abc import ABC, abstractmethod
+
+
+class QuantumPlatform(ABC):
+    pass
+
+
+class ExecutionEngine(ABC):
+    pass
+
+
+class ExecutionJob(ABC):
+    pass
---
+++ qcircuit_relayer/core/library.py
@@ -0,0 +1,3 @@
+
+class PlatformLibrary:
+    pass
